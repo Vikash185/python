497f1400c771dc0855e0a97c0c338f7128c15554
ViCcK <vksiwan456@gmail.com>
2025-04-08 14:11:55 +0530

    added files

--- triangle pattern.py
+++ triangle pattern.py
@@ -1,10 +1,24 @@
-rows = 20
-for i in range(1, rows):
-    num = 1
-    for j in range(rows, 0, -1):
-        if j > i:
-            print(" ", end=' ')
-        else:
-            print(num, end=' ')
-            num += 1
-    print(" ")
+# rows = 20
+# for i in range(1, rows):
+#     num = 1
+#     for j in range(rows, 0, -1):
+#         if j > i:
+#             print(" ", end=' ')
+#         else:
+#             print(num, end=' ')
+#             num += 1
+#     print(" ")
+import pandas as pd
+
+# Refined data (fixed the quotes and added matching elements)
+data = {
+    "Name": ["Amit", "Raju", "Ravi", "Rakesh"],  # Fixed the typo and added an extra name
+    "Age": [10, 55, 44, 30],  # Added the missing age value for Rakesh
+    "Salary": [100, 2000, 3000, 2500]  # Added the missing salary value for Rakesh
+}
+
+# Creating DataFrame
+df = pd.DataFrame(data)
+
+# Printing the DataFrame
+print(df)
